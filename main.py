3278a74eeff9649ad798ecf557ed43ff9bb1f3e5
Vikranth Udandarao <vikranth22570@iiitd.ac.in>
2023-12-28 04:06:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -90,8 +90,10 @@ def main():
         get_weather(api_key_weather, city_name, temperature_unit)
         get_news(api_key_news, city_name)
 
-    st.markdown("<p style='color:white; text-decoration:none;'>Made by <a href='https://github.com/Vikranth3140' style='color:white; text-decoration:none;'>Vikranth Udandarao</a></p>", unsafe_allow_html=True)
-    st.markdown("<p style='color:white; text-decoration:none;'><a href='https://github.com/Vikranth3140/CityInsight' style='color:white; text-decoration:none;'>Source Code</a></p>", unsafe_allow_html=True)
+    
 
 if __name__ == "__main__":
     main()
+
+st.markdown("<p style='color:white; text-decoration:none;'>Made by <a href='https://github.com/Vikranth3140' style='color:white; text-decoration:none;'>Vikranth Udandarao</a></p>", unsafe_allow_html=True)
+st.markdown("<p style='color:white; text-decoration:none;'><a href='https://github.com/Vikranth3140/CityInsight' style='color:white; text-decoration:none;'>Source Code</a></p>", unsafe_allow_html=True)
